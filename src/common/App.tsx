58200453ad3cabab8626ca56ea685f7547b81707
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-03-10 11:33:59 +0330

    reformat code

--- src/common/App.tsx
+++ src/common/App.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
-import "./assets/sass/App.scss";
+import './assets/sass/App.scss';
 
 function App() {
-  return <div></div>;
+    return <div></div>;
 }
 
 export default App;
