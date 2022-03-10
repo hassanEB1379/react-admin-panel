8954592f7ef0cd57bf810a74e5b9b246c28e1964
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-03-10 11:33:21 +0330

    add icon feature to Button

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,20 +1,33 @@
-import React from "react";
-import { ComponentStory, ComponentMeta } from "@storybook/react";
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import Button from "common/components/atoms/Button";
+import Button from 'common/components/atoms/Button';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: "Button",
-  component: Button,
+    title: 'Button',
+    component: Button,
 } as ComponentMeta<typeof Button>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+const Template: ComponentStory<typeof Button> = args => <Button {...args} />;
 
 export const ExampleButton = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 ExampleButton.args = {
-  label: "Button",
-  color: "primary",
+    label: 'Button',
+    color: 'primary',
+};
+
+export const WithIcon = Template.bind({});
+
+WithIcon.args = {
+    label: 'Button with Icon',
+    icon: 'fas fa-bold',
+};
+
+export const IconButton = Template.bind({});
+
+IconButton.args = {
+    icon: 'fas fa-bold',
 };
